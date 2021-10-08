d52d390ceb15560a765b4c55d51e57ba3039b568
Anoxia <87419299+Alost1@users.noreply.github.com>
2021-10-08 15:57:34 +0800

    Update Error.java

--- src/Error.java
+++ src/Error.java
@@ -1,5 +1,8 @@
 public class Error {
     public static void main(String[] args){
         System.out.println("Welcome);
+        System.out.println(1/0);
+        System.out.print("Celsius 35 is Fahrenheit degree");
+        System.out.println((9/5)*35+32);
     }
 }
